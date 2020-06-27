432214b1ba2a1fe290af5ba9bb2937662fdab76f
jxncdhc <dingx252@umn.edu>
2020-06-27 15:59:10 -0400

    add cmobined FN/LS for diff wald/LR  test

---
+++ R/LRTest_diff_phase_FN_LS.R
@@ -0,0 +1,124 @@
+##' Finite sample/Large sample Likelihood ratio test for differential phase.
+##'
+##' Test differential phase of circadian curve fitting using likelihood ratio test
+##' @title LRTest_diff_phase_FN_LS
+##' @param tt1 time vector of condition 1
+##' @param yy1 expression vector of condition 1
+##' @param tt2 time vector of condition 2
+##' @param yy2 expression vector of condition 2
+##' @param period Period of the since curve. Default is 24.
+##' @param type Type of likelihood ratio test to use, "FN" or "LS". Default is large sample.
+##' @return A list, see details below. 
+##' Formula 1: \eqn{yy = amp \times sin(2\pi/period \times (phase + tt)) + offset}
+##' Formula 2: \eqn{yy = A \times sin(2\pi/period \times tt) + B * cos(2*pi/period * tt) + offset}
+##' \item{phase_1}{phase estimate of the 1st data, phase is restricted in (0, period)}
+##' \item{phase_2}{phase estimate of the 2nd data, phase is restricted in (0, period)}
+##' \item{phase_c}{phase estimate pooling all data together, phase is restricted in (0, period)}
+##' \item{l0}{log likelihood under the null (same variance between the two groups)}
+##' \item{l1}{log likelihood under the alternative (different variance between the two groups)}
+##' \item{df}{degree of freedom for the LR test}
+##' \item{stat}{the LR statistics}
+##' \item{pvalue}{the p-value from the LR test}
+##' @author Caleb
+##' @export
+##' @examples
+##' set.seed(32608)
+##' n <- 50
+##' tt1 <- runif(n,0,24) 
+##' Amp1 <- 2
+##' Phase1 <- 6
+##' Offset1 <- 3
+##' yy1 <- Amp1 * sin(2*pi/24 * (tt1 + Phase1)) + Offset1 + rnorm(n,0,1)
+##' tt2 <- runif(n,0,24) 
+##' Amp2 <- 3
+##' Phase2 <- 5
+##' Offset2 <- 2
+##' yy2 <- Amp2 * sin(2*pi/24 * (tt2 + Phase2)) + Offset2 + rnorm(n,0,1)
+##' LRTest_diff_phase_FN_LS(tt1, yy1, tt2, yy2)
+
+
+LRTest_diff_phase_FN_LS <- function(tt1, yy1, tt2, yy2, period = 24,type="LS"){
+  n1 <- length(tt1)
+  stopifnot(n1 == length(yy1))
+  n2 <- length(tt2)
+  stopifnot(length(tt2) == length(yy2))
+  
+  w <- 2*pi/period
+  ## fit Ha, individual curves
+  par1 <- fitSinCurve(tt1,yy1)
+  sum_diffy_1_sq <- par1$rss
+  theta1 <- n1/sum_diffy_1_sq
+  
+  par2 <- fitSinCurve(tt2,yy2)
+  sum_diffy_2_sq <- par2$rss
+  theta2 <- n2/sum_diffy_2_sq
+  
+  l1_Ha <- 1/2 * n1 * log(theta1) - 1/2 * n1
+  l2_Ha <- 1/2 * n2 * log(theta2) - 1/2 * n2
+  
+  la <- unlist(l1_Ha + l2_Ha)
+  
+  beta0 <- c(par1$amp, 
+             (par1$phase + par2$phase)/2,
+             par1$offset,
+             theta1, 
+             par2$amp, 
+             par2$offset, 
+             theta2
+  )
+  
+  #opt_commonAmp(tt1, yy1, tt2, yy2, period=period)
+  #this_opt_commonAmp <- unlist(opt_commonAmp(tt1, yy1, tt2, yy2, period=period, parStart=beta0))
+  this_opt_commonPhase <- opt_commonPhase(tt1, yy1, tt2, yy2, period=period, parStart=beta0)
+  
+  ## fit H0, individual curves
+  ## extract parameters	  
+  amp_1 <- this_opt_commonPhase[1]
+  phase_c <- this_opt_commonPhase[2]
+  offset_1 <- this_opt_commonPhase[3]
+  theta_1 <- this_opt_commonPhase[4]
+  amp_2 <- this_opt_commonPhase[5]
+  offset_2 <- this_opt_commonPhase[6] 
+  theta_2 <- this_opt_commonPhase[7] 
+  
+  asin_1 <- sin(w * (tt1 + phase_c))
+  asin_2 <- sin(w * (tt2 + phase_c))
+  
+  yhat_1 <- amp_1 * asin_1 + offset_1
+  yhat_2 <- amp_2 * asin_2 + offset_2
+  
+  diffy_1 <- yy1-yhat_1
+  diffy_2 <- yy2-yhat_2
+  
+  l1a <- 1/2 * n1 * log(theta_1)
+  l1b <- - 1/2 * sum(diffy_1^2) * theta_1
+  
+  l2a <- 1/2 * n2 * log(theta_2)
+  l2b <- - 1/2 * sum(diffy_2^2) * theta_2
+  
+  l0 <- unlist(l1a + l1b + l2a + l2b)
+  
+  dfdiff <- 1
+  
+  if(type=="LS"){
+    pvalue <- pchisq(-2*(l0-la),dfdiff,lower.tail = F)
+  }
+  else if(type=="FN"){
+    LR_stat <- -2*(l0-la)
+    r <- 1
+    k <- 6
+    n <- n1+n2
+    Fstat <- (exp(LR_stat/n) - 1) * (n-k) / r
+    pvalue <- pf(Fstat,df1 = r, df2 = n-k, lower.tail = F)
+  }
+  
+  
+  res <- list(phase_1=par1$phase, phase_2=par2$phase, phase_c=phase_c, 
+              l0=l0, 
+              la=la, 
+              df = dfdiff, 
+              stat=-2*(l0-la), 
+              pvalue=pvalue)
+  return(res)
+}
+
---
+++ R/WaldTest_diff_sigma2_FN_LS.R
@@ -0,0 +1,112 @@
+##' Finite sample/Large sample Wald test for differential sigma2
+##'
+##' Test differential sigma2 of circadian curve fitting using Wald test
+##' @title WaldTest_diff_sigma2_FN_LS
+##' @param tt1 time vector of condition 1
+##' @param yy1 expression vector of condition 1
+##' @param tt2 time vector of condition 2
+##' @param yy2 expression vector of condition 2
+##' @param period Period of the since curve. Default is 24.
+##' @param type Type of Wald test to use, "FN" or "LS". Default is large sample.
+##' @return A list, see details below. 
+##' Formula 1: \eqn{yy = amp \times sin(2\pi/period \times (phase + tt)) + offset}
+##' Formula 2: \eqn{yy = A \times sin(2\pi/period \times tt) + B * cos(2*pi/period * tt) + offset}
+##' \item{sigma2_1}{variance estimate of the 1st data}
+##' \item{sigma2_2}{variance estimate of the 2nd data}
+##' \item{sigma2_C}{variance estimate pooling all data together}
+##' \item{df}{degree of freedom for the Wald test}
+##' \item{stat}{the Wald statistics}
+##' \item{pvalue}{the p-value from the Wald test}
+##' @author Caleb
+##' @export
+##' @examples
+##' set.seed(32608)
+##' n <- 50
+##' tt1 <- runif(n,0,24) 
+##' Amp1 <- 2
+##' Phase1 <- 6
+##' Offset1 <- 3
+##' yy1 <- Amp1 * sin(2*pi/24 * (tt1 + Phase1)) + Offset1 + rnorm(n,0,1)
+##' tt2 <- runif(n,0,24) 
+##' Amp2 <- 3
+##' Phase2 <- 5
+##' Offset2 <- 2
+##' yy2 <- Amp2 * sin(2*pi/24 * (tt2 + Phase2)) + Offset2 + rnorm(n,0,1)
+##' WaldTest_diff_sigma2_FN_LS(tt1, yy1, tt2, yy2)
+
+
+WaldTest_diff_sigma2_FN_LS <- function(tt1, yy1, tt2, yy2, period = 24){
+  n1 <- length(tt1)
+  stopifnot(n1 == length(yy1))
+  n2 <- length(tt2)
+  stopifnot(length(tt2) == length(yy2))
+  
+  w <- 2*pi/period
+  
+  fit1 <- fitSinCurve(tt1, yy1, period = 24)
+  fit2 <- fitSinCurve(tt2, yy2, period = 24)
+  sigma2_1 <- 1/n1 * fit1$rss
+  sigma2_2 <- 1/n2 * fit2$rss		
+  
+  ## fit Ha, individual curves
+  par1 <- fitSinCurve(tt1,yy1)
+  sigma2_1 <- 1/n1 * par1$rss
+  theta1 <- 1/sigma2_1
+  
+  par2 <- fitSinCurve(tt2,yy2)
+  sigma2_2 <- 1/n2 * par2$rss
+  theta2 <- 1/sigma2_2
+  
+  this_opt_commonSigma <- opt_commonSigma(tt1, yy1, tt2, yy2, period = period, parStart = c(sigma2_1,sigma2_2))
+  thetac <- 1/this_opt_commonSigma$sigma2_C
+  
+  beta_ha <- c(par1$amp, 
+               par1$phase, 
+               par1$offset, 
+               theta1, 
+               par2$amp,
+               par2$phase, 
+               par2$offset, 
+               theta2
+  )
+  #
+  beta_h0 <- c(par1$amp, 
+               par1$phase, 
+               par1$offset, 
+               thetac, 
+               par2$amp,
+               par2$phase, 
+               par2$offset, 
+               thetac
+  )
+  #
+  
+  I8 <- fisherInformation2(beta_ha, tt1, yy1, tt2, yy2, period=period)	
+  beta_diff <- matrix(beta_ha - beta_h0,ncol=1)	
+  #stat <- t(beta_diff) %*% I8 %*% beta_diff
+  
+  beta_diff2 <- beta_diff[c(4,8)]
+  I2 <- solve(solve(I8)[c(4,8),c(4,8)])
+  stat <- as.numeric(t(beta_diff2) %*% I2 %*% beta_diff2)
+  
+  dfdiff <- 1
+  
+  if(type=="LS"){
+    pvalue <- pchisq(stat,dfdiff,lower.tail = F)
+  }
+  else if(type=="FN"){
+    r <- 1
+    k <- 6
+    n <- n1+n2
+    Fstat <- stat*(n-k)/n/r
+    pvalue <- pf(Fstat,df1=r,df2=n-k,lower.tail = F)
+  }
+  
+  
+  res <- list(amp_1=par1$amp, amp_2=par2$amp, amp_c=this_opt_commonSigma$sigma2_C, 
+              df = dfdiff, 
+              stat = stat, 
+              pvalue = pvalue)
+  return(res)
+}
+
