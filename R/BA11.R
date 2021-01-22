0ae422ec19f21942db0a4e4054e42d0899accc3b
jxncdhc <haochengding@ufl.edu>
2021-01-22 16:55:29 -0500

    update BA11 doc

--- R/BA11.R
+++ R/BA11.R
@@ -1,11 +1,11 @@
 #' Human postmortem brain data (Brodmann's area 11 in the prefrontal cortex)
 #' 
-#' Brodmann’s Areas (BA) 11 Brain Aging Data
+#' @title Brodmann’s Areas (BA) 11 Brain Aging Data
 #'
 #' @usage 
 #' BA11
 #'
-#' @format A data frame with 8325 rows and 293 variables. First row are time of death (TOD) of each subject and rest of rows are gene expression values. First column is gene symbol. For rest of columns, each column represents one individual.
+#' @format A data frame with 8325 rows and 293 variables. First row is time of death (TOD) of each subject and rest of rows are gene expression values. First column is gene symbol. For rest of columns, each column represents one sample.
 #' 
 #' 
 #' @references 
