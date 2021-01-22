6499fd45cf5018f8f912904e369372e979d4aefe
jxncdhc <haochengding@ufl.edu>
2021-01-22 16:48:41 -0500

    update BA11 doc

--- R/BA11.R
+++ R/BA11.R
@@ -1,16 +1,15 @@
-#' Prices of 50,000 round cut diamonds.
-#' Human postmortem brain data (Brodmann's area 11 in the prefronttal cortex)
+#' @description  Human postmortem brain data (Brodmann's area 11 in the prefrontal cortex)
 #' 
-#' 
+#' @title Brodmann’s Areas (BA) 11 Brain Aging Data
+#'
+#' @usage 
+#' BA11
 #'
 #' @format A data frame with 8325 rows and 293 variables. First row are time of death (TOD) of each subject and rest of rows are gene expression values. First column is gene symbol. For rest of columns, each column represents one individual.
 #' 
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   
-#' }
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE71620}   
+#' 
 #' @references 
 #' Chen, C.-Y., Logan, R. W., Ma, T., Lewis, D. A., Tseng, G. C., Sibille, E., and McClung, C. A. (2016).Effects of aging on circadian patterns of gene expression in the human prefrontal cortex.Proceedings of the National Academy of Sciences, 113(1):206–211.
-#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE71620}
-"BA11"
+#' 
