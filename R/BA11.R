5d8b3ea42dd59e2ad3824cf1b6814b2b1fe41788
jxncdhc <haochengding@ufl.edu>
2021-01-22 16:51:50 -0500

    Update BA11.R

--- R/BA11.R
+++ R/BA11.R
@@ -8,8 +8,8 @@
 #' @format A data frame with 8325 rows and 293 variables. First row are time of death (TOD) of each subject and rest of rows are gene expression values. First column is gene symbol. For rest of columns, each column represents one individual.
 #' 
 #' 
-#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE71620}   
-#' 
 #' @references 
 #' Chen, C.-Y., Logan, R. W., Ma, T., Lewis, D. A., Tseng, G. C., Sibille, E., and McClung, C. A. (2016).Effects of aging on circadian patterns of gene expression in the human prefrontal cortex.Proceedings of the National Academy of Sciences, 113(1):206–211.
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE71620}  
 #' 
+"BA11"
