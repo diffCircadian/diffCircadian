aea408e2f9f86ef549bbc989544050fbf22b20ee
jxncdhc <haochengding@ufl.edu>
2021-01-23 17:58:29 -0500

    add BA11 old/young doc

---
+++ R/BA11old.R
@@ -0,0 +1,16 @@
+#' Human postmortem brain data (Brodmann's area 11 in the prefrontal cortex)
+#' 
+#' @title Brodmann’s Areas (BA) 11 Brain Aging Data Old Group (Age > 60)
+#'
+#' @usage 
+#' BA11old
+#'
+#' @format A data frame with 8325 rows and 38 variables. First row is time of death (TOD) of each subject and rest of rows are gene expression values. First column is gene symbol. For rest of columns, each column represents one sample.
+#' 
+#' 
+#' @references 
+#' Chen, C.-Y., Logan, R. W., Ma, T., Lewis, D. A., Tseng, G. C., Sibille, E., and McClung, C. A. (2016).Effects of aging on circadian patterns of gene expression in the human prefrontal cortex.Proceedings of the National Academy of Sciences, 113(1):206–211.
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE71620}  
+#' 
+"BA11old"
